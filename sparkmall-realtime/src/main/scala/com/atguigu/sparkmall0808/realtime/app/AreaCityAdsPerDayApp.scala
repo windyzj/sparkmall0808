120fa302ae061a564553f71e85b3f70fcc255ecf
zhangchen <windyzj@163.com>
2019-01-08 11:23:53 +0800

    需求八  更新  redis和updateStatebykey二选一

--- sparkmall-realtime/src/main/scala/com/atguigu/sparkmall0808/realtime/app/AreaCityAdsPerDayApp.scala
+++ sparkmall-realtime/src/main/scala/com/atguigu/sparkmall0808/realtime/app/AreaCityAdsPerDayApp.scala
@@ -15,10 +15,11 @@ object AreaCityAdsPerDayApp {
 //  把redis的key中对应值更新
 //  hash[key,[ day_area_city_ads,count]]
 
-  def updateAreaCityAdsPerDay(filteredAdsClickInfoDStream: DStream[AdsInfo],sparkContext: SparkContext ): Unit ={
+  def updateAreaCityAdsPerDay(filteredAdsClickInfoDStream: DStream[AdsInfo],sparkContext: SparkContext ): DStream[(String, Long)] ={
     //数据整理成为 RDD[day_area_city_ads,count] =
     val areaCityAdsPerDayCountDStream: DStream[(String, Long)] = filteredAdsClickInfoDStream.map { adsInfo => (adsInfo.getDayAreaCityAdsIdKey(), 1L)
     }.reduceByKey(_ + _)
+    //求总值 和 redis中的 incrby 二选一   推荐用redis
     val totalCountDStream: DStream[(String, Long)] = areaCityAdsPerDayCountDStream.updateStateByKey { (countSeq: Seq[Long], totalCount: Option[Long]) =>
       val sumCount: Long = countSeq.sum
       val newTotal: Long = totalCount.getOrElse(0L) + sumCount
@@ -31,11 +32,12 @@ object AreaCityAdsPerDayApp {
 
       val jedisClient: Jedis = RedisUtil.getJedisClient
       for ((key, count) <- totalCountArray ) {
-        jedisClient.hincrBy("day:area:city:adsCount",key,count )
+        jedisClient.hset("day:area:city:adsCount",key,count.toString )
       }
       jedisClient.close()
 
     }
+    totalCountDStream
    }
 
 }
